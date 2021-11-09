7151ee7ab2fed69969766d1d8473e49307429814
df229611 <63540184+df229611@users.noreply.github.com>
2021-11-09 20:42:39 +0200

    Delete #DFBbuilder directory

--- #DFBbuilder/resource/src/disableContextMenu.js
+++
@@ -1,9 +0,0 @@
-//Запрещаем контекстное меню
-function disableContextMenu(e) {
-	var clickedEl = (e==null) ? event.srcElement.tagName : e.target.tagName;
-	if (clickedEl == "CANVAS") {
-		
-		//alert("Context menu disabled.");
-		return false;
-	}
-}
